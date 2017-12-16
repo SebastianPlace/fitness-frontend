537817275034dc526b18f0e89bacadce2b848640
Sebastian Place <sebplace94@gmail.com>
2017-12-16 23:57:22 +0000

    Add styled components

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
-
-import './App.css';
+import styled from 'styled-components'
+import './App.css'
 import Routines from '../routines/Routines'
 
 const Home = () => (
@@ -13,21 +13,33 @@ const Home = () => (
     <h2>Home</h2>
   </div>
 )
+const NavBar = styled.div`
+  display: 'flex';
+  font-size: 1.5em;
+  padding: 2em;
+`
 
+const NavBarLink = styled(Link)`
+  margin-right: 50px;
+`
+
+const AppContentWrapper = styled.div`
+  padding: 2em;
+`
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/routines">Routines</Link></li>
-          </ul>
+          <NavBar>
+            <NavBarLink to="/">Home</NavBarLink>
+            <NavBarLink to="/routines">Routines</NavBarLink>
+          </NavBar>
     
-          <hr/>
-    
-          <Route exact path="/" component={Home}/>
-          <Route path="/routines" component={Routines}/>
+          <AppContentWrapper>
+            <Route exact path="/" component={Home}/>
+            <Route path="/routines" component={Routines}/>
+          </AppContentWrapper>
         </div>
       </Router>
     );
