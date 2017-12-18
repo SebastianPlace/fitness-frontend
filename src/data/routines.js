3632a690ba57c8c130813bd97ff8d5037713d231
Sebastian Place <sebplace94@gmail.com>
2017-12-18 20:51:28 +0000

    Add routine data

--- src/data/routines.js
+++ src/data/routines.js
@@ -1,8 +1,36 @@
 export default [
- {
-   name: 'push',
-   exercises: [
-     'Pushups'
-   ]
- }
-]
+  {
+    name: "push",
+    description: "Push routine covering chest, triceps and shoulders.",
+    duration: 60,
+    exercises: [
+      "Pushups",
+      "Archer pushups",
+      "Lateral raise",
+      "Dips",
+      "Monkey",
+      "Pike pushups",
+      "Chest fly",
+      "Military press",
+      "Tricep dips",
+      "Upright row"
+    ]
+  },
+  {
+    name: "pull",
+    description: "Pull routine covering back and biceps.",
+    duration: 60,
+    exercises: [
+      "Wide grip pullups",
+      "Bent over row",
+      "Narrow grip pullups",
+      "Superman",
+      "Chin ups",
+      "Headbangers",
+      "Rotations into bridge",
+      "One arm dead hang",
+      "Dead hang",
+      "Bicep curls"
+    ]
+  }
+];
